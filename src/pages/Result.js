f85aba3e878d4b274cf037ea4749982bf07cf2b6
Martin Gess <martingess@protonmail.ch>
2020-04-09 11:23:26 +0300

    ResultPage data presentation update

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -2,9 +2,9 @@ import React, { useRef } from 'react';
 import { Carousel, Col, Row, Icon } from 'antd';
 import { useSelector } from 'react-redux';
 import { Spin } from 'antd';
-import './Result.css'
-import imgPlaceholder from '../imgs/image-placeholder.jpg'
-
+import './Result.css';
+import imgPlaceholder from '../imgs/image-placeholder.jpg';
+import moment from 'moment';
 function Result(p) {
   const store = useSelector((state) => state);
   console.log(store);
@@ -24,7 +24,7 @@ function Result(p) {
     <>
       <Row type={'flex'} align={'middle'}>
         <Col span={10} offset={1}>
-          <CarouselComponent result={result}/>
+          <CarouselComponent result={result} />
         </Col>
         <Col span={10} offset={1}>
           <DataComponent result={result} />
@@ -36,33 +36,57 @@ function Result(p) {
 
 function DataComponent(p) {
   console.log(p);
-  return Object.keys(p.result).map((item) => (
-    <div key={item}>{p.result[item]}</div>
-  ));
+  return (
+    <>
+      <h2>Name: {p.result.name}</h2>
+      <p>Doctor: {p.result.doctorName}</p>
+      <p>Date: {moment(p.result.date).format('MMMM Do YYYY')}</p>
+      {p.result.note && <p>Notes: {p.result.note}</p>}
+    </>
+  );
 }
 
 function CarouselComponent(p) {
   const carousel = useRef(null);
-  const haveImg = !!p.result.imgsPaths[0]
-  const imgs = haveImg ? p.result.imgsPaths.map(img=> <img key={img} src={process.env.REACT_APP_BACKEND_PATH + img}/>) : <img src={imgPlaceholder}/>
-  const next = () => carousel.current.next()
-  const previous = () => carousel.current.prev()
+  const haveImg = !!p.result.imgsPaths[0];
+  const imgs = haveImg ? (
+    p.result.imgsPaths.map((img) => (
+      <img key={img} src={process.env.REACT_APP_BACKEND_PATH + img} />
+    ))
+  ) : (
+    <img src={imgPlaceholder} />
+  );
+  const next = () => carousel.current.next();
+  const previous = () => carousel.current.prev();
   return (
     <div className="result__carousel">
-    {imgs[1] ? <Icon type="left-circle" className="result__left-arrow" onClick={previous} /> : null}
-    <Carousel ref={carousel}
-      effect="fade"
-      style={{
-        textAlign: 'center',
-        height: '50vh',
-        lineHeight: '50vh',
-        background: '#364d79',
-        overflow: 'hidden',
-      }}>
+      {imgs[1] ? (
+        <Icon
+          type="left-circle"
+          className="result__left-arrow"
+          onClick={previous}
+        />
+      ) : null}
+      <Carousel
+        ref={carousel}
+        effect="fade"
+        style={{
+          textAlign: 'center',
+          height: '50vh',
+          lineHeight: '50vh',
+          background: '#364d79',
+          overflow: 'hidden',
+        }}>
         {imgs}
       </Carousel>
-      {imgs[1] ? <Icon type="right-circle" className="result__right-arrow" onClick={next} /> : null}
-      </div>
+      {imgs[1] ? (
+        <Icon
+          type="right-circle"
+          className="result__right-arrow"
+          onClick={next}
+        />
+      ) : null}
+    </div>
   );
 }
 
